1b1c5d695ecb2d044c559ecd4d7406d5557c0cd1
Khthonian <anansicorp@tuta.io>
2023-07-05 10:12:49 +0100

    Complete Test 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof num1 != "number" || typeof num2 != "number") {
+        return "ERROR";
+    }
 
+    let lowBound = Math.min(num1, num2);
+    let highBound = Math.max(num1, num2);
+
+    if (lowBound < 0 || highBound < 0) {
+        return "ERROR";
+    }
+
+    let finalSum = 0;
+
+    for (let i = lowBound; i <= highBound; i++) {
+        finalSum += i;
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
