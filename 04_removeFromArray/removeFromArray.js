439aa058248f8df7f5db74093a0c8166e3bcc9fe
Khthonian <anansicorp@tuta.io>
2023-07-05 09:45:38 +0100

    Complete Test 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeArgs) {
+    for (let i = 0; i < removeArgs.length; i++) {
+        let key = array.indexOf(removeArgs[i]);
 
+        if (key !== -1) {
+            array.splice(key, 1);
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
