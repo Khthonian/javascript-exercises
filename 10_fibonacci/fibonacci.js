af45349e3fcc97ea276b31ccaca6cf25ee63a835
Khthonian <anansicorp@tuta.io>
2023-07-13 13:45:35 +0100

    Complete Test 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number < 0) return "OOPS";
+    
+    let a = 1, b = 0, c;
+    number--;
 
+    while (number >= 0) {
+        c = a;
+        a = a + b;
+        b = c;
+        number--;
+    }
+    return b;
 };
 
 // Do not edit below this line
