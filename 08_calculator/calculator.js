e16f7e9b6859c1ee08ca92e7f367c874cde15971
Khthonian <anansicorp@tuta.io>
2023-07-13 13:20:34 +0100

    Complete Test 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+	return args.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let sum = 1;
+  for (let arg of args) {
+    sum *= arg;
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a == 0) return 1;
+
+  let sum = 1;
+  for (let i = a; i > 0; i--) {
+    sum *= i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
